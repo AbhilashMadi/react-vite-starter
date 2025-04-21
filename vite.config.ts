f476199aa214e88d8956c9b50ccfd4f44de380d8
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-04-21 23:28:40 +0530

    feat: path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: { "@": resolve(__dirname, "src") },
+  },
 });
